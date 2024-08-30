02e18ac4c54d9e196a46347d79dfb39ea4630821
felipeFleite <felipefracalossileite@gmail.com>
2024-08-30 17:01:18 -0400

    salve

--- client/src/Home/index.jsx
+++ client/src/Home/index.jsx
@@ -1,9 +1,30 @@
 import { Link } from 'react-router-dom';
 import '../globals.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
 import Footers from "../Footer.jsx"
 export default function Home() {
     return (
-        
+        <>
+        <Carousel
+        infiniteLoop
+        autoPlay
+        usekeyboardArrows
+        showArrows={true}
+        showStatus={false}
+        showThumbs={false}
+        dynamicHeight
+        >
+
+        <div>
+        <img src ="https://static.estantevirtual.com.br/bnn/l_estantevirtual/2024-08-26/5501_full%2520desk%2520quero%2520tudo.png"/>
+        </div>
+
+        <div>
+        <img src ="https://static.estantevirtual.com.br/bnn/l_estantevirtual/2024-04-04/9508_homedesk.psicologia.png"/>
+        </div>
+
+        </Carousel>
         <div className='container'>
             
             <h2 id="titulo">ESTANTE VIRTUAL</h2>
@@ -19,6 +40,8 @@ export default function Home() {
                 </Link>
             </div>
             <Footers/>
+            
         </div>
+        </>
     );
 }
